84f8695232ac1d62e11149fbd3f6479d801ebde5
pulistar <camilo.pulistar@campusucc.edu.co>
2025-03-26 18:55:01 -0500

    divinos espumosos

---
+++ scripts.js
@@ -0,0 +1,5 @@
+document.getElementById('contact-form')?.addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('¡Gracias por ponerte en contacto con nosotros!');
+  });
+  
